4fec979288c90b3bbabc652db9bb1ac6ec10a69f
Jesus Illescas <76405615+jillesca@users.noreply.github.com>
2024-02-01 17:13:33 +0000

    Breaking: Improving the isis neighbor metrics (#8)

--- ncpeek/parsers/cisco_ios_xe_isis_oper.py
+++ ncpeek/parsers/cisco_ios_xe_isis_oper.py
@@ -1,79 +1,125 @@
-from typing import Optional
-from dataclasses import dataclass, field
+from collections import defaultdict
+from typing import List, Dict
 from ncpeek.netconf_parsers import Parser
 from ncpeek.netconf_devices import NetconfDevice
 
 
-@dataclass
 class ISISStatsIOSXEParser(Parser):
     """
     A parser for ISIS stats for IOSXE devices.
     """
 
-    device: NetconfDevice = None
-    isis_interfaces_adj_up: int = 0
-    stats: list = field(default_factory=list)
-    netconf_filter_id: Optional[str] = None
+    def __init__(self, device: NetconfDevice = None):
+        """
+        Initialize the parser.
+
+        Args:
+            device (NetconfDevice, optional): The device to parse data for. Defaults to None.
+        """
+        self.device = device
+        self.parsed_stats = []
 
     def parse(
         self,
-        data_to_parse: dict,
+        data_to_parse: Dict,
         device: NetconfDevice,
         netconf_filter_id: str,
-    ) -> list[dict]:
+    ) -> List[Dict]:
         """
         Parse ISIS stats data.
 
         Args:
             data (Dict): The data to parse.
             device (NetconfDevice): The device the data is related to.
-            netconf_filter_id (str): The filter ID used for netconf.
+            filter_id (str): The filter ID used for netconf.
 
         Returns:
             List[Dict]: The parsed data.
         """
         self.device = device
-        self._extract_neighbor_stats(data=data_to_parse)
-        self.netconf_filter_id = netconf_filter_id
-        return self.stats
+        self.filter_id = netconf_filter_id
+        grouped_data = self._group_data_by_system_id(data_to_parse)
+        return self._prepare_output_data(grouped_data)
 
-    def _extract_neighbor_stats(self, data: dict) -> None:
-        isis_instances: dict = data["data"]["isis-oper-data"]["isis-instance"]
+    def _group_data_by_system_id(self, data: Dict) -> Dict:
+        """
+        Group data by system-id.
 
-        for key, isis_instance in isis_instances.items():
-            if "isis-neighbor" in key:
-                self._count_adjcencies(instance=isis_instance)
-                self._extract_metadata(instance=isis_instance)
+        Args:
+            data (Dict): The data to group.
 
-    def _count_adjcencies(self, instance: list) -> None:
-        """If you have 2 isis interfaces data comes inside a list.
-        if you shutdown one interface and only 1 is up,
-        the data will not come insdie a list."""
-        if isinstance(instance, list):
-            for neighbor in instance:
-                if "isis-adj-up" in neighbor["state"]:
-                    self.isis_interfaces_adj_up += 1
-        else:
-            if "isis-adj-up" in instance["state"]:
-                self.isis_interfaces_adj_up += 1
+        Returns:
+            Dict: The grouped data.
+        """
+        grouped_data = defaultdict(list)
+        for neighbor in data["data"]["isis-oper-data"]["isis-instance"][
+            "isis-neighbor"
+        ]:
+            grouped_data[neighbor["system-id"]].append(
+                self._create_neighbor_dict(neighbor)
+            )
+        return grouped_data
 
-    def _extract_metadata(self, instance: list) -> None:
-        if isinstance(instance, list):
-            for neighbor in instance:
-                self.stats.append(
-                    self._get_neighbor_metadata(neighbor=neighbor)
-                )
-        else:
-            self.stats.append(self._get_neighbor_metadata(neighbor=instance))
+    @staticmethod
+    def _create_neighbor_dict(neighbor: Dict) -> Dict:
+        """
+        Create a dictionary for a neighbor.
 
-    def _get_neighbor_metadata(self, neighbor: dict) -> dict:
+        Args:
+            neighbor (Dict): The neighbor data.
+
+        Returns:
+            Dict: The neighbor dictionary.
+        """
         return {
-            "isis_interfaces_adj_up": self.isis_interfaces_adj_up,
-            "isis_status": neighbor["state"],
-            "neighbor_id": neighbor["system-id"].replace(" ", "_"),
-            "field": self.netconf_filter_id,
-            "device": self.device.hostname,
-            "ip": self.device.host,
             "interface_name": neighbor["if-name"],
-            "ipv4_address": neighbor["ipv4-address"],
+            "level": neighbor["level"],
+            "local_ipv4_address": neighbor["ipv4-address"],
+            "isis_status": neighbor["state"],
+            "holdtime": neighbor["holdtime"],
         }
+
+    def _prepare_output_data(self, grouped_data: Dict) -> List[Dict]:
+        """
+        Prepare the output data.
+
+        Args:
+            grouped_data (Dict): The grouped data.
+
+        Returns:
+            List[Dict]: The output data.
+        """
+        output_data = [
+            {
+                "isis_neighbors_count": len(grouped_data),
+                "isis-neighbors": self._prepare_neighbor_data(grouped_data),
+                "field": self.filter_id,
+                "device": self.device.hostname,
+                "ip": self.device.host,
+            }
+        ]
+        return output_data
+
+    def _prepare_neighbor_data(self, grouped_data: Dict) -> List[Dict]:
+        """
+        Prepare the neighbor data.
+
+        Args:
+            grouped_data (Dict): The grouped data.
+
+        Returns:
+            List[Dict]: The neighbor data.
+        """
+        return [
+            {
+                "system-id": system_id,
+                "local_interfaces_status": interfaces,
+                "neighbor_status": 1
+                if any(
+                    interface["isis_status"] == "isis-adj-up"
+                    for interface in interfaces
+                )
+                else 0,
+            }
+            for system_id, interfaces in grouped_data.items()
+        ]
